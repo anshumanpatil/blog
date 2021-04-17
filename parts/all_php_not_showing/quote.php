6238fb33a86ffa659418162dc308ab826096a514
anshuman.patil <anshuman.patil@sumasoft.net>
2021-04-17 16:31:25 +0530

    complete new app

--- parts/all_php_not_showing/quote.php
+++
@@ -1,5 +0,0 @@
-<h5 class="my-3">Quote Example:</h5>
-<blockquote class="blockquote m-lg-5 py-3 pl-4 px-lg-5">
-    <p class="mb-2">You might not think that programmers are artists, but programming is an extremely creative profession. It's logic-based creativity.</p>
-    <footer class="blockquote-footer">John Romero</footer>
-</blockquote>
--- parts/all_php_not_showing/video.php
+++
@@ -1,5 +0,0 @@
-<h3 class="mt-5 mb-3">Video Example</h3>
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
-<div class="embed-responsive embed-responsive-16by9">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/hnCmSXCZEpU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>					
-</div>
